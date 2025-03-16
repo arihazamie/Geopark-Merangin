80f18a43eda30a2ce1ad28fc304bcbd58fff2acd
Ari Hazamie <arihzmii@gmail.com>
2025-03-16 22:19:53 +0700

    fix: some conflict

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
+// prismaLocal.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-=======
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
-
-const prisma = new PrismaClient().$extends(withAccelerate());
-
+// Instance untuk lingkungan lokal tanpa ekstensi tambahan
+const prisma = new PrismaClient();
 export default prisma;
->>>>>>> a7be68471d91463051f140769bc8565235b8325e
